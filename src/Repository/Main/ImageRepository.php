06f8dc50001d4b1dbe9b73235f80783511b5b2b8
Shanbora <chanbora@logilink.fr>
2023-03-13 16:57:54 +0100

    move folder image

--- src/Repository/Main/ImageRepository.php
+++ src/Repository/Main/ImageRepository.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Repository;
+namespace App\Repository\Main;
 
-use App\Entity\Image;
+use App\Entity\Main\Image;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
