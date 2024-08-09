33111aba9731390198b54fcb3ccee52b67f08f17
shanbora <chanbora@logilink.fr>
2024-08-09 16:01:35 +0200

    init menu

--- assets/user/js/app.js
+++ assets/user/js/app.js
@@ -3,29 +3,10 @@ import '../css/app.scss';
 const routes = require('@publicFolder/js/fos_js_routes.json');
 import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min';
 
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Notifications } from "@tailwindComponents/Modules/Notifications/Notifications";
+import Menu from "@tailwindFunctions/menu";
+import Toastr from "@tailwindFunctions/toastr";
 
 Routing.setRoutingData(routes);
 
-let notifs = document.getElementById("notifs_list");
-if(notifs){
-    createRoot(notifs).render(<Notifications />)
-}
-
-menu();
-
-function menu() {
-    let btn = document.querySelector('.nav-mobile');
-    if(btn){
-        btn.addEventListener('click', function () {
-            let content = document.querySelector('.nav-content');
-            if(content.classList.contains('active')){
-                content.classList.remove('active');
-            }else{
-                content.classList.add('active');
-            }
-        })
-    }
-}
+Menu.menuListener();
+Toastr.flashes();
