eea779c3de9229cc810d00e1ee09de9e687d5d5e
shanbora <chanbora@logilink.fr>
2025-01-08 17:57:51 +0100

    menu tab

--- assets/user/js/pages/components/Documentations/Products/Lotys/CommGerance/CommGerance.jsx
+++ assets/user/js/pages/components/Documentations/Products/Lotys/CommGerance/CommGerance.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { cn } from "@shadcnComponents/lib/utils"
 
 import { General } from "@userPages/Documentations/Products/Lotys/CommGerance/Fiche/General";
 
 export function CommGerance() {
-	return <div className="absolute top-0 left-0 w-full h-full bg-gray-950/30">
-		<General />
-	</div>
+	const [tab, setTab] = useState(0);
+
+	let styleTab = "cursor-pointer px-2 py-1.5";
+	let styleTabInactive = "hover:bg-blue-100 transition-colors";
+	let styleTabActive = "bg-blue-500 text-slate-50 font-medium";
+
+	let tabs = [
+		{ value: 0, label: "Général" },
+		{ value: 1, label: "Description" },
+		{ value: 2, label: "Photos" },
+		{ value: 3, label: "Conditions" },
+	];
+
+	return <>
+		<div className="absolute top-0 left-0 w-[870px] h-[668px] rounded-md  bg-gray-950/30">
+			<General />
+		</div>
+		<div className="bg-white border shadow rounded-md w-full">
+			<div className="py-2 border-b font-semibold text-center">Navigation</div>
+			<div>
+				{tabs.map((item, index) => {
+					return <div key={index}>
+						<div className={cn(
+							styleTab,
+							tab === item.value ? styleTabActive : styleTabInactive,
+							index === (tabs.length - 1) ? "rounded-b-md" : ""
+						)}
+							 onClick={() => setTab(item.value)}>
+							{item.label}
+						</div>
+					</div>
+				})}
+			</div>
+		</div>
+	</>
 }
 
 export function getInputStyle () {
