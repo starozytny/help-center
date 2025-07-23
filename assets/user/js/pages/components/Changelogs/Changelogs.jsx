d1d797299301f74273c9da8f0e2bffb9165709be
shanbora <chanbora@logilink.fr>
2025-07-23 11:21:03 +0200

    duplicate changelog

--- assets/user/js/pages/components/Changelogs/Changelogs.jsx
+++ assets/user/js/pages/components/Changelogs/Changelogs.jsx
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
 
+import axios from "axios";
 import Routing from '@publicFolder/bundles/fosjsrouting/js/router.min.js';
 
 import Sort from "@commonFunctions/sort";
 import List from "@commonFunctions/list";
+import Validateur from "@commonFunctions/validateur";
+import Formulaire from "@commonFunctions/formulaire";
 
 import { ChangelogsList } from "@userPages/Changelogs/ChangelogsList";
 
+import { Modal } from "@tailwindComponents/Elements/Modal";
+import { Input } from "@tailwindComponents/Elements/Fields";
 import { Search } from "@tailwindComponents/Elements/Search";
+import { Button } from "@tailwindComponents/Elements/Button";
 import { ModalDelete } from "@tailwindComponents/Shortcut/Modal";
 import { LoaderElements } from "@tailwindComponents/Elements/Loader";
 import { Pagination, TopSorterPagination } from "@tailwindComponents/Elements/Pagination";
 
+const URL_INDEX_ELEMENTS = "user_help_changelogs_index";
 const URL_GET_DATA = "intern_api_help_changelogs_list";
 const URL_DELETE_ELEMENT = "intern_api_help_changelogs_delete";
+const URL_DUPLICATE_ELEMENT = "intern_api_help_changelogs_duplicate";
 
 const SESSION_PERPAGE = "help.user.perpage.changelogs";
 
@@ -26,11 +34,14 @@ export class Changelogs extends Component {
 			currentPage: 0,
 			sorter: Sort.compareNumVersionInverseThenDateAtInverse,
 			loadingData: true,
-			element: null
+			element: null,
+			numVersion: "",
+			errors: []
 		}
 
 		this.pagination = React.createRef();
 		this.delete = React.createRef();
+		this.duplicate = React.createRef();
 	}
 
 	componentDidMount = () => {
@@ -71,12 +82,55 @@ export class Changelogs extends Component {
 
 	handleModal = (identifiant, elem) => {
 		this[identifiant].current.handleClick();
-		this.setState({ element: elem });
+		this.setState({ element: elem, numVersion: Formulaire.setValue(elem.numVersion) });
+	}
+
+	handleChange = (e) => {
+		this.setState({ [e.currentTarget.name]: e.currentTarget.value })
+	}
+
+	handleDuplicate = () => {
+		const { productSlug } = this.props;
+		const { element, numVersion } = this.state;
+
+		this.setState({ errors: [] });
+
+		let paramsToValidate = [
+			{ type: "text", id: 'numVersion', value: numVersion },
+		];
+
+		let validate = Validateur.validateur(paramsToValidate);
+
+		if(numVersion === element.numVersion){
+			validate.code = false;
+			validate.errors.push({
+				name: "numVersion",
+				message: "Veuillez changer de numéro de version."
+			})
+		}
+
+		if (!validate.code) {
+			Formulaire.showErrors(this, validate);
+		} else {
+			let self = this;
+			Formulaire.loader(true);
+			axios({ method: "POST", url: Routing.generate(URL_DUPLICATE_ELEMENT, { id: element.id }), data: {numVersion: numVersion} })
+				.then(function (response) {
+					location.href = Routing.generate(URL_INDEX_ELEMENTS, { p_slug: productSlug, h: response.data.id });
+				})
+				.catch(function (error) {
+					Formulaire.displayErrors(self, error);
+					Formulaire.loader(false);
+				})
+			;
+		}
 	}
 
 	render () {
 		const { highlight, productSlug } = this.props;
-		const { data, currentData, element, loadingData, perPage, currentPage } = this.state;
+		const { data, currentData, element, loadingData, perPage, currentPage, errors, numVersion } = this.state;
+
+		let params0 = { errors: errors, onChange: this.handleChange };
 
 		return <>
 			{loadingData
@@ -103,6 +157,25 @@ export class Changelogs extends Component {
 								 onUpdateList={this.handleUpdateList}>
 						Êtes-vous sûr de vouloir supprimer définitivement ce changelog ?
 					</ModalDelete>
+
+
+					<Modal ref={this.duplicate} identifiant="duplicate" maxWidth={414}
+						   title={element ? "Duplication de " + element.numVersion + " - " + element.name : ""}
+						   content={element
+							   ? <div>
+								   <p>
+									   Êtes-vous sûr de vouloir supprimer définitivement ce changelog : <b>{element ? element.numVersion : ""}</b> ?
+								   </p>
+								   <div className="mt-2">
+									   <Input identifiant="numVersion" valeur={numVersion} {...params0} placeholder="ex: 3000.0.0.1P001">
+										   Numéro de version + patch
+									   </Input>
+								   </div>
+							   </div>
+							   : <LoaderElements />
+					       }
+						   footer={<Button type="blue" onClick={this.handleDuplicate}>Dupliquer</Button>}
+					/>
 				</>
 			}
 		</>
