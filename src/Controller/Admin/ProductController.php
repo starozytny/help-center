ff3b6b7d68574cb2dbac8062022442842f101c5d
shanbora <chanbora@logilink.fr>
2025-11-19 11:40:08 +0100

    init product

---
+++ src/Controller/Admin/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/changelogs', name: 'admin_changelogs_')]
+class ProductController extends AbstractController
+{
+    #[Route('/', name: 'index', options: ['expose' => true])]
+    public function index(Request $request): Response
+    {
+        return $this->render('admin/pages/changelogs/index.html.twig', ['highlight' => $request->query->get('h')]);
+    }
+}
